504505b08a31f3fa40fae45a18c87fa0d0622731
Chris Jensen <chris@goodforus.info>
2016-07-06 10:27:13 +0800

    Write initial tests

--- test/attr_redactor_test.rb
+++ test/attr_redactor_test.rb
@@ -1,52 +1,54 @@
 # encoding: UTF-8
 require_relative 'test_helper'
-
-class SillyEncryptor
-  def self.silly_encrypt(options)
-    (options[:value] + options[:some_arg]).reverse
-  end
-
-  def self.silly_decrypt(options)
-    options[:value].reverse.gsub(/#{options[:some_arg]}$/, '')
-  end
-end
-
 class User
-  extend AttrEncrypted
-  self.attr_encrypted_options[:key] = Proc.new { |user| SECRET_KEY } # default key
-  self.attr_encrypted_options[:mode] = :per_attribute_iv_and_salt
+  extend AttrRedactor
 
-  attr_encrypted :email, :without_encoding, :key => SECRET_KEY
-  attr_encrypted :password, :prefix => 'crypted_', :suffix => '_test'
-  attr_encrypted :ssn, :key => :secret_key, :attribute => 'ssn_encrypted'
-  attr_encrypted :credit_card, :encryptor => SillyEncryptor, :encrypt_method => :silly_encrypt, :decrypt_method => :silly_decrypt, :some_arg => 'test'
-  attr_encrypted :with_encoding, :key => SECRET_KEY, :encode => true
-  attr_encrypted :with_custom_encoding, :key => SECRET_KEY, :encode => 'm'
-  attr_encrypted :with_marshaling, :key => SECRET_KEY, :marshal => true
-  attr_encrypted :with_true_if, :key => SECRET_KEY, :if => true
-  attr_encrypted :with_false_if, :key => SECRET_KEY, :if => false
-  attr_encrypted :with_true_unless, :key => SECRET_KEY, :unless => true
-  attr_encrypted :with_false_unless, :key => SECRET_KEY, :unless => false
-  attr_encrypted :with_if_changed, :key => SECRET_KEY, :if => :should_encrypt
+  redact_hash = {
+  	:ssn => :remove,
+  	:email => :digest,
+  	:medical_notes => :encrypt
+  }
+  
+  encryption_key = 'really, really secure, no one will guess it'
+  digest_salt = 'digest salt'
 
-  attr_encryptor :aliased, :key => SECRET_KEY
-
-  attr_accessor :salt
-  attr_accessor :should_encrypt
-
-  def initialize(email: nil)
-    self.email = email
-    self.salt = Time.now.to_i.to_s
-    self.should_encrypt = true
-  end
-
-  def secret_key
-    SECRET_KEY
-  end
+  attr_redactor :data, redact: redact_hash,
+  					   encryption_key: encryption_key,
+  					   digest_salt: digest_salt
+  attr_redactor :attr1, :attr2, redact: redact_hash,
+  					   encryption_key: encryption_key,
+  					   digest_salt: digest_salt
+  attr_redactor :extra, redact: redact_hash,
+  					   encryption_key: encryption_key,
+  					   digest_salt: digest_salt,
+  					   prefix: 'totally_',
+  					   suffix: '_secret'
+  attr_redactor :secret, redact: redact_hash,
+  					   encryption_key: encryption_key,
+  					   digest_salt: digest_salt,
+  					   attribute: 'renamed_redacted_attribute',
+  attr_accessor :name
 end
 
-class Admin < User
-  attr_encrypted :testing
+class AlternativeClass
+  extend AttrRedactor
+ 
+  redact_hash = { :name => :remove,
+  				  :uid => :digest,
+  				  :address => :encrypt }
+  
+  encryption_key = 'a completely different key, equally unguessable'
+  digest_salt = 'saltier'
+
+  attr_redactor_options redact: redact_hash,
+  						encryption_key: encryption_key,
+  						digest_salt: digest_salt
+  						
+  attr_redactor :secret
+end
+
+class SubClass < AlternativeClass
+  attr_redactor :testing
 end
 
 class SomeOtherClass
@@ -56,175 +58,141 @@ class SomeOtherClass
   end
 end
 
-class YetAnotherClass
-  extend AttrEncrypted
-  self.attr_encrypted_options[:encode_iv] = false
-
-  attr_encrypted :email, :key => SECRET_KEY
-  attr_encrypted :phone_number, :key => SECRET_KEY, mode: Proc.new { |thing| thing.mode }, encode_iv: Proc.new { |thing| thing.encode_iv }, encode_salt: Proc.new { |thing| thing.encode_salt }
-
-  def initialize(email: nil, encode_iv: 'm', encode_salt: 'm', mode: :per_attribute_iv_and_salt)
-    self.email = email
-    @encode_iv = encode_iv
-    @encode_salt = encode_salt
-    @mode = mode
-  end
-
-  attr_reader :encode_iv, :encode_salt, :mode
-end
-
 class AttrEncryptedTest < Minitest::Test
   def setup
     @iv = SecureRandom.random_bytes(12)
   end
+  
+  def data_to_redact
+    {
+		:ssn => 'my secret ssn',
+		:email => 'personal@email.com',
+		:medical_notes => 'This is very personal and private'
+    }
+  end
+  
+  # Unredacting data will always look slightly different since removed
+  # keys will remain removed, and digested will remain digested
+  # This hash contains what a structure should look like after redaction
+  def unredacted_data
+    r = HashRedactor::HashRedactor.new(redact: User.redact_hash,
+					   encryption_key: User.encryption_key,
+  					   digest_salt: User.digest_salt)
 
-  def test_should_store_email_in_encrypted_attributes
-    assert User.encrypted_attributes.include?(:email)
+	r.decrypt(r.redact(data_to_redact))
   end
 
-  def test_should_not_store_salt_in_encrypted_attributes
-    refute User.encrypted_attributes.include?(:salt)
+  def test_should_store_datal_in_redacted_attributes
+    assert User.redacted_attributes.include?(:data)
   end
 
-  def test_attr_encrypted_should_return_true_for_email
-    assert User.attr_encrypted?('email')
+  def test_attr_redacted_should_return_true_for_data
+    assert User.attr_redacted?('data')
   end
 
-  def test_attr_encrypted_should_not_use_the_same_attribute_name_for_two_attributes_in_the_same_line
-    refute_equal User.encrypted_attributes[:email][:attribute], User.encrypted_attributes[:without_encoding][:attribute]
+  def test_attr_redacted_should_not_use_the_same_attribute_name_for_two_attributes_in_the_same_line
+    refute_equal User.redacted_attributes[:attr1][:attribute], User.redacted_attributes[:attr2][:attribute]
   end
 
-  def test_attr_encrypted_should_return_false_for_salt
-    assert !User.attr_encrypted?('salt')
+  def test_attr_redacted_should_return_false_for_name
+    assert !User.attr_redacted?('name')
   end
 
-  def test_should_generate_an_encrypted_attribute
-    assert User.new.respond_to?(:encrypted_email)
+  def test_should_generate_redacted_attribute
+    assert User.new.respond_to?(:redacted_data)
   end
 
-  def test_should_generate_an_encrypted_attribute_with_a_prefix_and_suffix
-    assert User.new.respond_to?(:crypted_password_test)
+  def test_should_generate_redacted_attribute_with_a_prefix_and_suffix
+    assert User.new.respond_to?(:totally_extra_secret)
   end
 
   def test_should_generate_an_encrypted_attribute_with_the_attribute_option
-    assert User.new.respond_to?(:ssn_encrypted)
+    assert User.new.respond_to?(:renamed_redacted_attribute)
   end
 
-  def test_should_not_encrypt_nil_value
-    assert_nil User.encrypt_email(nil, iv: @iv)
+  def test_should_not_change_nil_value
+    assert_nil User.redact_data(nil, iv: @iv)
   end
 
-  def test_should_not_encrypt_empty_string
-    assert_equal '', User.encrypt_email('', iv: @iv)
+  def test_should_redact_data
+    refute_nil User.redact_data(data_to_redact)
+    refute_equal data_to_redact, User.redact_data(data_to_redact)
   end
 
-  def test_should_encrypt_email
-    refute_nil User.encrypt_email('test@example.com', iv: @iv)
-    refute_equal 'test@example.com', User.encrypt_email('test@example.com', iv: @iv)
+  def test_should_use_hash_redactor
+    r = HashRedactor::HashRedactor.new(redact: User.redact_hash,
+					   encryption_key: User.encryption_key,
+  					   digest_salt: User.digest_salt)
+    assert_equal r.redact(data_to_redact), User.redact_data(data_to_redact)
   end
 
-  def test_should_encrypt_email_when_modifying_the_attr_writer
+  def test_should_redact_when_modifying_the_attr_writer
     @user = User.new
-    assert_nil @user.encrypted_email
-    @user.email = 'test@example.com'
-    refute_nil @user.encrypted_email
-    iv = @user.encrypted_email_iv.unpack('m').first
-    salt = @user.encrypted_email_salt[1..-1].unpack('m').first
-    assert_equal User.encrypt_email('test@example.com', iv: iv, salt: salt), @user.encrypted_email
+    assert_nil @user.redacted_data
+    @user.data = data_to_redact
+    refute_nil @user.redacted_data
+    assert_equal User.redact_data(data_to_redact), @user.redacted_data
   end
 
-  def test_should_not_decrypt_nil_value
-    assert_nil User.decrypt_email(nil, iv: @iv)
+  def test_should_not_unredact_nil_value
+    assert_nil User.unredact_data(nil)
   end
 
-  def test_should_not_decrypt_empty_string
-    assert_equal '', User.decrypt_email('', iv: @iv)
+  def test_should_unredact
+    redacted_data = User.redact_data(data_to_redact)
+    refute_equal data_to_redact, redacted_data
+    assert_equal r.decrypt(redacted_data), User.unredact_data(redacted_data)
   end
 
-  def test_should_decrypt_email
-    encrypted_email = User.encrypt_email('test@example.com', iv: @iv)
-    refute_equal 'test@test.com', encrypted_email
-    assert_equal 'test@example.com', User.decrypt_email(encrypted_email, iv: @iv)
-  end
-
-  def test_should_decrypt_email_when_reading
+  def test_should_unredact_when_reading
     @user = User.new
-    assert_nil @user.email
-    iv = @user.encrypted_email_iv.unpack('m').first
-    salt = @user.encrypted_email_salt[1..-1].unpack('m').first
-    @user.encrypted_email = User.encrypt_email('test@example.com', iv: iv, salt: salt)
-    assert_equal 'test@example.com', @user.email
+    assert_nil @user.data
+    @user.redacted_data = User.redact_data(data_to_redact)
+    assert_equal unredacted_data, @user.data
   end
 
-  def test_should_encrypt_with_encoding
-    assert_equal User.encrypt_with_encoding('test', iv: @iv), [User.encrypt_without_encoding('test', iv: @iv)].pack('m')
-  end
-
-  def test_should_decrypt_with_encoding
-    encrypted = User.encrypt_with_encoding('test', iv: @iv)
-    assert_equal 'test', User.decrypt_with_encoding(encrypted, iv: @iv)
-    assert_equal User.decrypt_with_encoding(encrypted, iv: @iv), User.decrypt_without_encoding(encrypted.unpack('m').first, iv: @iv)
-  end
-
-  def test_should_encrypt_with_custom_encoding
-    assert_equal User.encrypt_with_encoding('test', iv: @iv), [User.encrypt_without_encoding('test', iv: @iv)].pack('m')
-  end
-
-  def test_should_decrypt_with_custom_encoding
-    encrypted = User.encrypt_with_encoding('test', iv: @iv)
-    assert_equal 'test', User.decrypt_with_encoding(encrypted, iv: @iv)
-    assert_equal User.decrypt_with_encoding(encrypted, iv: @iv), User.decrypt_without_encoding(encrypted.unpack('m').first, iv: @iv)
-  end
-
-  def test_should_encrypt_with_marshaling
+  def test_should_redact_when_hash_changed
     @user = User.new
-    @user.with_marshaling = [1, 2, 3]
-    refute_nil @user.encrypted_with_marshaling
+    @user.data = data_to_redact
+    old_email_digest = @user.redacted_data[:email_digest]
+    @user.data[:email] = 'new_email@example.com'
+    refute_equal @user.redacted_data[:email_digest], old_email_digest
   end
 
-  def test_should_use_custom_encryptor_and_crypt_method_names_and_arguments
-    assert_equal SillyEncryptor.silly_encrypt(:value => 'testing', :some_arg => 'test'), User.encrypt_credit_card('testing')
-  end
+  def test_should_use_options_found_in_the_attr_redactor_options_attribute
+    @user = AlternativeClass.new
+    
+    alternative_redact_data = {
+		:name => 'Mr Murray',
+  		:uid => 124356677,
+  		:address => '1 Over the Rainbow, Somewhere'
+    }
+    
+    assert_nil @user.secret
+    @user.secret = alternative_redact_data
+    refute_nil @user.secret
+    
+    r = HashRedactor::HashRedactor.new(redact: AlternativeClass.redact_hash,
+					   encryption_key: AlternativeClass.encryption_key,
+  					   digest_salt: AlternativeClass.digest_salt)
 
-  def test_should_evaluate_a_key_passed_as_a_symbol
-    @user = User.new
-    assert_nil @user.ssn_encrypted
-    @user.ssn = 'testing'
-    refute_nil @user.ssn_encrypted
-    encrypted =  Encryptor.encrypt(:value => 'testing', :key => SECRET_KEY, :iv => @user.ssn_encrypted_iv.unpack("m").first, :salt => @user.ssn_encrypted_salt.unpack("m").first )
-    assert_equal encrypted, @user.ssn_encrypted
-  end
+	redacted = r.redact(alternative_redact_data)
 
-  def test_should_evaluate_a_key_passed_as_a_proc
-    @user = User.new
-    assert_nil @user.crypted_password_test
-    @user.password = 'testing'
-    refute_nil @user.crypted_password_test
-    encrypted = Encryptor.encrypt(:value => 'testing', :key => SECRET_KEY, :iv => @user.crypted_password_test_iv.unpack("m").first, :salt =>  @user.crypted_password_test_salt.unpack("m").first)
-    assert_equal encrypted, @user.crypted_password_test
-  end
-
-  def test_should_use_options_found_in_the_attr_encrypted_options_attribute
-    @user = User.new
-    assert_nil @user.crypted_password_test
-    @user.password = 'testing'
-    refute_nil @user.crypted_password_test
-    encrypted = Encryptor.encrypt(:value => 'testing', :key => SECRET_KEY, :iv => @user.crypted_password_test_iv.unpack("m").first, :salt => @user.crypted_password_test_salt.unpack("m").first)
-    assert_equal encrypted, @user.crypted_password_test
+    assert_equal redacred, @user.redacted_secret
   end
 
   def test_should_inherit_encrypted_attributes
-    assert_equal [User.encrypted_attributes.keys, :testing].flatten.collect { |key| key.to_s }.sort, Admin.encrypted_attributes.keys.collect { |key| key.to_s }.sort
+    assert_equal [AlternativeClass.redacted_attributes.keys, :testing].flatten.collect { |key| key.to_s }.sort, SubClass.redacted_attributes.keys.collect { |key| key.to_s }.sort
   end
 
   def test_should_inherit_attr_encrypted_options
-    assert !User.attr_encrypted_options.empty?
-    assert_equal User.attr_encrypted_options, Admin.attr_encrypted_options
+    assert !SubClass.attr_redacted_options.empty?
+    assert_equal AlternativeClass.attr_redacted_options, Admin.attr_redacted_options
   end
 
   def test_should_not_inherit_unrelated_attributes
-    assert SomeOtherClass.attr_encrypted_options.empty?
-    assert SomeOtherClass.encrypted_attributes.empty?
+    assert SomeOtherClass.attr_redacted_options.empty?
+    assert SomeOtherClass.redacted_attributes.empty?
   end
 
   def test_should_evaluate_a_symbol_option
@@ -248,55 +216,6 @@ class AttrEncryptedTest < Minitest::Test
     assert_equal class_string, SomeOtherClass.new.send(:evaluate_attr_encrypted_option, class_string)
   end
 
-  def test_should_encrypt_with_true_if
-    @user = User.new
-    assert_nil @user.encrypted_with_true_if
-    @user.with_true_if = 'testing'
-    refute_nil @user.encrypted_with_true_if
-    encrypted = Encryptor.encrypt(:value => 'testing', :key => SECRET_KEY, :iv => @user.encrypted_with_true_if_iv.unpack("m").first, :salt => @user.encrypted_with_true_if_salt.unpack("m").first)
-    assert_equal encrypted, @user.encrypted_with_true_if
-  end
-
-  def test_should_not_encrypt_with_false_if
-    @user = User.new
-    assert_nil @user.encrypted_with_false_if
-    @user.with_false_if = 'testing'
-    refute_nil @user.encrypted_with_false_if
-    assert_equal 'testing', @user.encrypted_with_false_if
-  end
-
-  def test_should_encrypt_with_false_unless
-    @user = User.new
-    assert_nil @user.encrypted_with_false_unless
-    @user.with_false_unless = 'testing'
-    refute_nil @user.encrypted_with_false_unless
-    encrypted = Encryptor.encrypt(:value => 'testing', :key => SECRET_KEY, :iv => @user.encrypted_with_false_unless_iv.unpack("m").first, :salt => @user.encrypted_with_false_unless_salt.unpack("m").first)
-    assert_equal encrypted,  @user.encrypted_with_false_unless
-  end
-
-  def test_should_not_encrypt_with_true_unless
-    @user = User.new
-    assert_nil @user.encrypted_with_true_unless
-    @user.with_true_unless = 'testing'
-    refute_nil @user.encrypted_with_true_unless
-    assert_equal 'testing', @user.encrypted_with_true_unless
-  end
-
-  def test_should_work_with_aliased_attr_encryptor
-    assert User.encrypted_attributes.include?(:aliased)
-  end
-
-  def test_should_always_reset_options
-    @user = User.new
-    @user.with_if_changed = "encrypt_stuff"
-
-    @user = User.new
-    @user.should_encrypt = false
-    @user.with_if_changed = "not_encrypted_stuff"
-    assert_equal "not_encrypted_stuff", @user.with_if_changed
-    assert_equal "not_encrypted_stuff", @user.encrypted_with_if_changed
-  end
-
   def test_should_cast_values_as_strings_before_encrypting
     string_encrypted_email = User.encrypt_email('3', iv: @iv)
     assert_equal string_encrypted_email, User.encrypt_email(3, iv: @iv)
@@ -305,89 +224,16 @@ class AttrEncryptedTest < Minitest::Test
 
   def test_should_create_query_accessor
     @user = User.new
-    assert !@user.email?
-    @user.email = ''
-    assert !@user.email?
-    @user.email = 'test@example.com'
-    assert @user.email?
+    assert !@user.data?
+    @user.data = ''
+    assert !@user.data?
+    @user.data = data_to_redact
+    assert @user.data?
   end
 
-  def test_should_vary_iv_per_attribute
+  def test_should_redact_immediately
     @user = User.new
-    @user.email = 'email@example.com'
-    @user.password = 'p455w0rd'
-    refute_equal @user.encrypted_email_iv, @user.crypted_password_test_iv
-  end
-
-  def test_should_generate_iv_per_attribute_by_default
-    thing = YetAnotherClass.new(email: 'thing@thing.com')
-    refute_nil thing.encrypted_email_iv
-  end
-
-  def test_should_vary_iv_per_instance
-    @user1 = User.new
-    @user1.email = 'email@example.com'
-    @user2 = User.new
-    @user2.email = 'email@example.com'
-    refute_equal @user1.encrypted_email_iv, @user2.encrypted_email_iv
-    refute_equal @user1.encrypted_email, @user2.encrypted_email
-  end
-
-  def test_should_vary_salt_per_attribute
-    @user = User.new
-    @user.email = 'email@example.com'
-    @user.password = 'p455w0rd'
-    refute_equal @user.encrypted_email_salt, @user.crypted_password_test_salt
-  end
-
-  def test_should_vary_salt_per_instance
-    @user1 = User.new
-    @user1.email = 'email@example.com'
-    @user2 = User.new
-    @user2.email = 'email@example.com'
-    refute_equal @user1.encrypted_email_salt, @user2.encrypted_email_salt
-  end
-
-  def test_should_not_generate_salt_per_attribute_by_default
-    thing = YetAnotherClass.new(email: 'thing@thing.com')
-    assert_nil thing.encrypted_email_salt
-  end
-
-  def test_should_decrypt_second_record
-    @user1 = User.new
-    @user1.email = 'test@example.com'
-
-    @user2 = User.new
-    @user2.email = 'test@example.com'
-
-    assert_equal 'test@example.com', @user1.decrypt(:email, @user1.encrypted_email)
-  end
-
-  def test_should_specify_the_default_algorithm
-    assert YetAnotherClass.encrypted_attributes[:email][:algorithm]
-    assert_equal YetAnotherClass.encrypted_attributes[:email][:algorithm], 'aes-256-gcm'
-  end
-
-  def test_should_not_encode_iv_when_encode_iv_is_false
-    email = 'thing@thing.com'
-    thing = YetAnotherClass.new(email: email)
-    refute thing.encrypted_email_iv =~ base64_encoding_regex
-    assert_equal thing.email, email
-  end
-
-  def test_should_base64_encode_iv_by_default
-    phone_number = '555-555-5555'
-    thing = YetAnotherClass.new
-    thing.phone_number = phone_number
-    assert thing.encrypted_phone_number_iv =~ base64_encoding_regex
-    assert_equal thing.phone_number, phone_number
-  end
-
-  def test_should_generate_unique_iv_for_every_encrypt_operation
-    user = User.new
-    user.email = 'initial_value@test.com'
-    original_iv = user.encrypted_email_iv
-    user.email = 'revised_value@test.com'
-    refute_equal original_iv, user.encrypted_email_iv
+    @user.data = data_to_redact
+    assert_equal unredacted_data, @user.data
   end
 end
