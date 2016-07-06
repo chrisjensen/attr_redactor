36f9733dfc083d684be71c5d0c32eaaf48837eb9
Chris Jensen <chris@goodforus.info>
2016-07-06 10:27:37 +0800

    reset version to 0.1.0

--- lib/attr_redactor/version.rb
+++ lib/attr_redactor/version.rb
@@ -1,9 +1,9 @@
-module AttrEncrypted
+module AttrRedactor
   # Contains information about this gem's version
   module Version
-    MAJOR = 3
-    MINOR = 0
-    PATCH = 1
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 0
 
     # Returns a version string by joining <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt> with <tt>'.'</tt>
     #
